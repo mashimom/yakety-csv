53a68d369c0b80805f0940c1eb6d25e79a10f9d2
Marco Shimomoto <mashimom@gmail.com>
2021-01-26 22:42:06 +0100

    un-nested from template

--- lib/src/integrationTest/resources/SpockConfig.groovy
+++
@@ -1,9 +0,0 @@
-runner {
-	optimizeRunOrder true
-	filterStackTrace true
-}
-spockReports {
-	set 'com.athaydes.spockframework.report.testSourceRoots': 'src/integrationTest/groovy'
-	set 'com.athaydes.spockframework.report.showCodeBlocks': true
-	set 'com.athaydes.spockframework.report.outputDir': 'build/reports/spock-reports/integrationTest'
-}
--- lib/src/test/resources/SpockConfig.groovy
+++
@@ -1,8 +0,0 @@
-runner {
-	optimizeRunOrder true
-	filterStackTrace true
-}
-spockReports {
-	set 'com.athaydes.spockframework.report.showCodeBlocks': true
-	set 'com.athaydes.spockframework.report.outputDir': 'build/reports/spock-reports/test'
-}
