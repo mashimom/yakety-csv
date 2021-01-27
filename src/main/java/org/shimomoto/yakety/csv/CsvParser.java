e4226d8c6d476909ef35c6b18999e71da0ed4cf2
Marco Shimomoto <mashimom@gmail.com>
2021-01-27 04:25:00 +0100

    refactored code

--- src/main/java/org/shimomoto/yakety/csv/CsvParser.java
+++ src/main/java/org/shimomoto/yakety/csv/CsvParser.java
@@ -3,9 +3,9 @@ package org.shimomoto.yakety.csv;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -14,36 +14,45 @@ import java.util.stream.Stream;
 @Slf4j
 public class CsvParser {
 
-	static final Pattern lineRegex =
+	//TODO: fix for windows line endings
+	//TODO: fix for quoted line breaks
+	private static final Pattern lineRegex = Pattern.compile("(?<=^|\\n)(.*?)(?=\\n|$)");
+	private static final Pattern fieldRegex =
 					Pattern.compile("(?:(?:(?<=^|,)(?<!\")\"(.*?)(?<!\")\"(?=,|$))|(?:(?<=^|,)(.*?)(?=,|$)))+");
 
-	public @NotNull Stream<Stream<String>> parse(String content) {
-		final BufferedReader br = new BufferedReader(new StringReader(content));
-		final Runnable safeClose = () -> {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		};
-
-		return br.lines()
-						.map(l -> lineSplit(lineRegex, l))
-						.onClose(safeClose);
+	private static Stream<String> lineSplit(Scanner scanner) {
+		return scanner
+						.findAll(lineRegex)
+						.map(MatchResult::group);
 	}
 
-	private static Stream<String> lineSplit(@SuppressWarnings("SameParameterValue")
-	                                        @NotNull final Pattern pattern,
-	                                        @NotNull final String line) {
+	private static Stream<String> fieldSplit(@SuppressWarnings("SameParameterValue")
+	                                         @NotNull final Pattern pattern,
+	                                         @NotNull final String line) {
 		return new Scanner(line)
 						.findAll(pattern)
 						.map(MatchResult::group);
+	}
 
-//		final Matcher matcher = pattern.matcher(line);
-//		final Stream.Builder<String> stb = Stream.builder();
-//		while (matcher.find()) {
-//			stb.add(matcher.group());
-//		}
-//		return stb.build();
+	public @NotNull Stream<Stream<String>> parse(final String content) {
+		return parse(new Scanner(content));
+	}
+
+	public @NotNull Stream<Stream<String>> parse(final File file) {
+		try {
+			return parse(new Scanner(file));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return Stream.empty();
+	}
+
+	public @NotNull Stream<Stream<String>> parse(final InputStream input) {
+		return parse(new Scanner(input));
+	}
+
+	public @NotNull Stream<Stream<String>> parse(Scanner scanner) {
+		return lineSplit(scanner)
+						.map(l -> fieldSplit(fieldRegex, l));
 	}
 }
