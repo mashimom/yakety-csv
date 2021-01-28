54e631f02977f1dbf71473fec57b536f895302ae
Marco Shimomoto <mashimom@gmail.com>
2021-01-28 23:10:25 +0100

    added implementations that output to map w/o index

--- src/main/java/org/shimomoto/yakety/csv/BaseCsvParser.java
+++ src/main/java/org/shimomoto/yakety/csv/BaseCsvParser.java
@@ -1,15 +1,12 @@
 package org.shimomoto.yakety.csv;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringEscapeUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.shimomoto.yakety.csv.api.CsvParser;
 
 import java.io.File;
@@ -21,7 +18,6 @@ import java.util.regex.Pattern;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @EqualsAndHashCode
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter(AccessLevel.PROTECTED)
 @Slf4j
 abstract class BaseCsvParser<S> implements CsvParser<S> {
@@ -29,9 +25,17 @@ abstract class BaseCsvParser<S> implements CsvParser<S> {
 	Pattern fieldRegex;
 	char quote; //TODO: move to column by column option
 	boolean trim; //TODO: move to column by column option
+	Pattern escapeQuoteRegex;
 
-	@NotNull
-	protected static Pattern getPattern(final @NotNull Locale parserLocale, final String separator, final char quote) {
+	protected BaseCsvParser(final Pattern lineBreakRegex, final Pattern fieldRegex, final char quote, final boolean trim) {
+		this.lineBreakRegex = lineBreakRegex;
+		this.fieldRegex = fieldRegex;
+		this.quote = quote;
+		this.trim = trim;
+		this.escapeQuoteRegex = Pattern.compile(String.format("%c{2}", quote));
+	}
+
+	protected static Pattern getPattern(final Locale parserLocale, final String separator, final char quote) {
 		final String separatorString = StringEscapeUtils.escapeJava(separator);
 
 		return Pattern.compile(String.format(
@@ -41,11 +45,11 @@ abstract class BaseCsvParser<S> implements CsvParser<S> {
 						quote, quote, quote, quote, quote));
 	}
 
-	public @NotNull S parse(final String content) {
+	public S parse(final String content) {
 		return parseLines(new Scanner(content));
 	}
 
-	public @NotNull S parse(final File file) {
+	public S parse(final File file) {
 		try {
 			return parseLines(new Scanner(file));
 		} catch (final FileNotFoundException e) {
@@ -56,17 +60,16 @@ abstract class BaseCsvParser<S> implements CsvParser<S> {
 
 	protected abstract S empty();
 
-	public @NotNull S parse(final InputStream input) {
+	public S parse(final InputStream input) {
 		return parseLines(new Scanner(input));
 	}
 
-	protected abstract S parseLines(Scanner scanner);
+	protected abstract S parseLines(final Scanner scanner);
 
-	protected String unquote(@Nullable final String field) {
+	protected String unquote(final String field) {
 		if (field == null) {
 			return null;
 		}
-		final Pattern escapeQuoteRegex = Pattern.compile(String.format("%c{2}", quote));
 		return escapeQuoteRegex.matcher(StringUtils.unwrap(field, quote))
 						.replaceAll(Character.toString(quote));
 	}
---
+++ src/test/groovy/org/shimomoto/yakety/csv/CsvToRowIndexedTextMapParserSpec.groovy
@@ -0,0 +1,118 @@
+package org.shimomoto.yakety.csv
+
+import java.util.stream.Collectors
+
+import spock.lang.Specification
+
+class CsvToRowIndexedTextMapParserSpec extends Specification {
+
+	@SuppressWarnings('GroovyAccessibility')
+	def "Create new parser from default configuration"() {
+		given:
+		def config = FileFormatConfiguration.builder().build()
+		def cols = ['lala', 'lele', 'lili', 'lolo']
+
+		when:
+		def parser = CsvToRowIndexedTextMapParser.from(config, 'i', cols)
+
+		then:
+		parser.lineBreakRegex.toString() == '\\n(?=([^"]*"[^"]*")*[^"]*$)'
+		parser.fieldRegex.toString() == ',(?=([^"]*"[^"]*")*[^"]*$)'
+		parser.quote == '"' as char
+		!parser.trim
+		parser.columnNames == ['i', *cols]
+	}
+
+	@SuppressWarnings('GroovyAccessibility')
+	def "Create new parser from custom configuration"() {
+		given:
+		def config = FileFormatConfiguration.builder()
+						.parserLocale(Locale.forLanguageTag('pt-BR'))
+						.lineBreak('\r\n')
+						.separator(';' as char)
+						.quote('|' as char)
+						.trim(true)
+						.build()
+		def cols = ['lala', 'lele', 'lili', 'lolo']
+
+		when:
+		def parser = CsvToRowIndexedTextMapParser.from(config, 'index', cols)
+
+		then:
+		parser.lineBreakRegex.toString() == '\\r\\n(?=([^|]*|[^|]*|)*[^|]*$)'
+		parser.fieldRegex.toString() == ';(?=([^|]*|[^|]*|)*[^|]*$)'
+		parser.quote == '|' as char
+		parser.trim
+		parser.columnNames == ['index', *cols]
+	}
+
+	def "Parse a simple sample with defaults"() {
+		given:
+		def cols = ['Title', 'Release date', 'Phase', 'Film/TV', 'In-universe year']
+		def parser = CsvToRowIndexedTextMapParser.from(FileFormatConfiguration.builder().trim(true).build(), '#', cols)
+		def content = '''\
+								Title,                              Release date, Phase, Film/TV, In-universe year
+								Iron Man,                           2008-05-02,   1,     Film,    2008
+								The Incredible Hulk,                2008-06-13,   1,     Film,    2009
+								Iron Man 2,                         2010-04-30,   1,     Film,    2009
+								Thor,                               2011-04-27,   1,     Film,    2009
+								Captain America: The First Avenger, 2011-07-29,   1,     Film,    1942-1945
+								The Avengers,                       2012-04-26,   1,     Film,    2010
+								'''.stripIndent()
+		when:
+		List<Map<String, String>> result = parser.parse(content).collect(Collectors.toList())
+
+		then:
+		result.size() == 6
+		result[0] == ['#': '1', 'Title': 'Iron Man', 'Release date': '2008-05-02', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '2008']
+		result[1] == ['#': '2', 'Title': 'The Incredible Hulk', 'Release date': '2008-06-13', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '2009']
+		result[2] == ['#': '3', 'Title': 'Iron Man 2', 'Release date': '2010-04-30', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '2009']
+		result[3] == ['#': '4', 'Title': 'Thor', 'Release date': '2011-04-27', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '2009']
+		result[4] == ['#': '5', 'Title': 'Captain America: The First Avenger', 'Release date': '2011-07-29', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '1942-1945']
+		result[5] == ['#': '6', 'Title': 'The Avengers', 'Release date': '2012-04-26', 'Phase': '1', 'Film/TV': 'Film', 'In-universe year': '2010']
+	}
+
+	def "Parse a complex sample"() {
+		given:
+		def cols = ['Make', 'Model', 'Description', 'Price']
+		def parser = CsvToRowIndexedTextMapParser.from(FileFormatConfiguration.builder().build(), 'ndx', cols)
+		def content = '''\
+						Make,Model,Description,Price
+						Dell,P3421W,"Dell 34, Curved, USB-C Monitor",2499.00
+						Dell,"","Alienware 38 Curved ""Gaming Monitor""",  6699.00
+						Samsung,,"49"" Dual QHD, QLED, HDR1000",6199.00
+						Samsung,,"  Promotion! Special Price
+						49"" Dual QHD, QLED, HDR1000  ",4999.00
+						'''.stripIndent()
+
+		when:
+		List<Map<String, String>> result = parser.parse(content).collect(Collectors.toList())
+
+		then:
+		result.size() == 4
+		and: 'line #0'
+		result[0]['ndx'] == '1'
+		result[0]['Make'] == 'Dell'
+		result[0]['Model'] == 'P3421W'
+		result[0]['Description'] == 'Dell 34, Curved, USB-C Monitor'
+		result[0]['Price'] == '2499.00'
+		and: 'line #1'
+		result[1]['ndx'] == '2'
+		result[1]['Make'] == 'Dell'
+		result[1]['Model'] == ''
+		result[1]['Description'] == 'Alienware 38 Curved "Gaming Monitor"'
+		result[1]['Price'] == '  6699.00'
+		and: 'line #2'
+		result[2]['ndx'] == '3'
+		result[2]['Make'] == 'Samsung'
+		result[2]['Model'] == ''
+		result[2]['Description'] == '49" Dual QHD, QLED, HDR1000'
+		result[2]['Price'] == '6199.00'
+		and: 'line #3'
+		result[3]['ndx'] == '4'
+		result[3]['Make'] == 'Samsung'
+		result[3]['Model'] == ''
+		result[3]['Description'] == '  Promotion! Special Price\n49" Dual QHD, QLED, HDR1000  '
+		result[3]['Price'] == '4999.00'
+	}
+}
