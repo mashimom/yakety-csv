c30284f9cf212f79fc706e3a7a7d7e86dd17d6d7
Marco Shimomoto <mashimom@gmail.com>
2021-01-28 18:14:55 +0100

    Base implementation

--- src/main/java/org/shimomoto/yakety/csv/CsvToTextParser.java
+++ src/main/java/org/shimomoto/yakety/csv/CsvToTextParser.java
@@ -1,21 +1,13 @@
 package org.shimomoto.yakety.csv;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.text.StringEscapeUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.shimomoto.yakety.csv.api.CsvParser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -23,77 +15,40 @@ import java.util.stream.Stream;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @EqualsAndHashCode
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j
-public class CsvToTextParser implements CsvParser<Stream<List<String>>> {
-	Pattern lineBreakRegex;
-	Pattern fieldRegex;
-	char quote; //TODO: move to column by column option
-	boolean trim; //TODO: move to column by column option
-
-	@NotNull
-	private static Pattern getPattern(final @NotNull Locale parserLocale, final String separator, final char quote) {
-		final String separatorString = StringEscapeUtils.escapeJava(separator);
-
-		return Pattern.compile(String.format(
-						parserLocale,
-						"%s(?=([^%1c]*%1c[^%1c]*%1c)*[^%1c]*$)",
-						separatorString,
-						quote, quote, quote, quote, quote));
+public class CsvToTextParser extends BaseCsvParser<Stream<List<String>>> implements CsvParser<Stream<List<String>>> {
+	private CsvToTextParser(final Pattern lineBreakRegex, final Pattern fieldRegex, final char quote, final boolean trim) {
+		super(lineBreakRegex, fieldRegex, quote, trim);
 	}
 
 	public static CsvToTextParser from(final FileFormatConfiguration config) {
 		return new CsvToTextParser(
-						getPattern(config.getParserLocale(), config.getLineBreak(), config.getQuote()),
-						getPattern(config.getParserLocale(), Character.toString(config.getSeparator()), config.getQuote()),
+						BaseCsvParser.getPattern(config.getParserLocale(), config.getLineBreak(), config.getQuote()),
+						BaseCsvParser.getPattern(config.getParserLocale(), Character.toString(config.getSeparator()), config.getQuote()),
 						config.getQuote(),
 						config.isTrim());
 	}
 
-	public @NotNull Stream<List<String>> parse(final String content) {
-		return parseLines(new Scanner(content));
-	}
-
-	public @NotNull Stream<List<String>> parse(final File file) {
-		try {
-			return parseLines(new Scanner(file));
-		} catch (final FileNotFoundException e) {
-			log.error("Unable to read file: {}", file);
-		}
+	@Override
+	protected Stream<List<String>> empty() {
 		return Stream.empty();
 	}
 
-	public @NotNull Stream<List<String>> parse(final InputStream input) {
-		return parseLines(new Scanner(input));
-	}
-
-	private Stream<List<String>> parseLines(final Scanner scanner) {
-		return scanner.useDelimiter(lineBreakRegex)
+	@Override
+	protected Stream<List<String>> parseLines(final Scanner scanner) {
+		return scanner.useDelimiter(super.getLineBreakRegex())
 						.tokens()
 						.map(this::getFieldsFromLine);
 	}
 
 	@NotNull
-	private List<String> getFieldsFromLine(final String line) {
+	protected List<String> getFieldsFromLine(final String line) {
 		try (final Scanner sc = new Scanner(line)) {
-			return sc.useDelimiter(fieldRegex)
+			return sc.useDelimiter(super.getFieldRegex())
 							.tokens()
-							.map(this::mayTrim)
-							.map(this::unquote)
+							.map(super::mayTrim)
+							.map(super::unquote)
 							.collect(Collectors.toList());
 		}
-	}
-
-	private String unquote(@Nullable final String field) {
-		if (field == null) {
-			return null;
-		}
-		final Pattern escapeQuoteRegex = Pattern.compile(String.format("%c{2}", quote));
-		return escapeQuoteRegex.matcher(StringUtils.unwrap(field, quote))
-						.replaceAll(Character.toString(quote));
-	}
-
-	private String mayTrim(final String field) {
-		return trim ? field.trim() : field;
 	}
 }
