251be4083e83c74420c2624935b394258580ae94
Marco Shimomoto <mashimom@gmail.com>
2021-01-31 19:57:16 +0100

    extra methods in the columndefinition

--- src/main/java/org/shimomoto/yakety/csv/api/ColumnDefinition.java
+++ src/main/java/org/shimomoto/yakety/csv/api/ColumnDefinition.java
@@ -5,4 +5,12 @@ public interface ColumnDefinition {
 	int getOrder();
 
 	String getName();
+
+	default boolean mustTrim() {
+		return false;
+	}
+
+	default boolean quoted() {
+		return false;
+	}
 }
