d3a02fd03fed3df047854222ccde06b042840edb
Marco Shimomoto <mashimom@gmail.com>
2021-02-03 21:42:57 +0100

    base map parser

---
+++ src/main/java/org/shimomoto/yakety/csv/field/BaseMapDelegatedParser.java
@@ -0,0 +1,20 @@
+package org.shimomoto.yakety.csv.field;
+
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.shimomoto.yakety.csv.field.api.FieldParser;
+import org.shimomoto.yakety.csv.field.api.MapFieldParser;
+
+import java.util.Map;
+
+@Value
+public class BaseMapDelegatedParser<F,C> implements MapFieldParser<C, F> {
+	@NotNull FieldParser<F> delegate;
+	@NotNull C key;
+
+	@Override
+	public @Nullable F parse(@NotNull final Map<C, String> map) {
+		return delegate.parse(map.get(key));
+	}
+}
